3c483da3f41bed4b35e4193737279268f16a68fa
VixenLaRuk <vixenlaruk@gmail.com>
2025-10-30 15:11:31 -0700

    add temporary function

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,3 +1,45 @@
 public class BankAccount {
 
+    private double balance;
+    StringBuilder transactions;
+
+    BankAccount() { // default constructor
+        balance = 0.0;
+        transactions = new StringBuilder();
+
+    }
+
+    BankAccount(double amount) {
+        balance = amount;
+        transactions = new StringBuilder();
+    }
+
+    int deposit(double amount) {
+        if (amount < 0) {
+            return -1;
+        }
+        else {
+            balance += amount;
+            return 1;
+        }
+    }
+
+    /**
+     * 
+     * @param amount
+     * @return
+     */
+    int withdraw(double amount) throws Exception {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    void temp() {
+        
+    }
 }
